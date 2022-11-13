bb32b25658ede581b8fc8a93121ec71c58e1ebf4
MyFootHurts <112666133+HenryLukey@users.noreply.github.com>
2022-11-13 11:21:30 +0000

    Changed top left home redirect to have pointer cursor on hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         // Div to contain all nav elements
         <div className="flex items-center justify-center bg-light top-0 z-50 sticky border-b bg-lightColour border-darkColour">
-            <h1 className="text-darkColour text-3xl font-black font-roboto px-4 select-none" onClick={returnToHome}>YourBarCart</h1>
+            <h1 className="text-darkColour text-3xl font-black font-roboto px-4 select-none hover:cursor-pointer" onClick={returnToHome}>YourBarCart</h1>
             <div className="flex-1"></div>
             <div className="flex-1"></div>
             {/* Settings button, this is a dropdown of options */}
